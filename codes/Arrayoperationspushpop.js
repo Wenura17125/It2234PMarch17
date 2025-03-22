a01bd16292bfe5d409c26ed0364844090e273551
Wenura <95497272+Wenura17125@users.noreply.github.com>
2025-03-22 20:21:15 +0530

    Delete codes directory

--- codes/Arrayoperationspushpop.js
+++
@@ -1,8 +0,0 @@
-//Array operations
-//push pop
-arr3 = ['a','b','d'];
-console.log(arr3);
-arr3.push('e');
-console.log(arr3);
-arr3.pop();
-console.log(arr3);
--- codes/printarrayusingforloop.js
+++
@@ -1,5 +0,0 @@
-let score = [84, 92, 75, 68, 55, 95, 66, 89, 87, 74];
-for(let i=0;i< score.length;i++)
-{
-    console.log(score[i]);
-}
